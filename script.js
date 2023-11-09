5efbd0aa355e1203c08f13e0d72d1b1784d775ec
junior vargas <juniorvargascovarrub@my.whccd.edu>
2023-11-08 18:55:54 -0800

    I did Fizz Buzz

--- script.js
+++ script.js
@@ -1,9 +1,20 @@
 let outputString = "#"
-for (let x = 1; x < 5; x++){
+for (let x = 1; x < 5; x++) {
    console.log(outputString);
    outputString += "#";
 }
-for(let x = 4; x > 1; x--){
-    outputString = outputString.substring(0,-1);
+for(let x = 4; x > 1; x--) {
     console.log(outputString);
+    outputString -= "#";
+}
+for(let i = 1; i <= 100; i++) {
+    if (i % 3 == 0 && i % 5 ==0) {
+        console.log("FizzBuss");
+    } else if (i % 3 == 0) {
+        console.log("Fizz");
+    } else if (i % 5 == 0){
+        console.log("Buzz");
+    } else {
+        console.log(i)
+    }
 }
